0aaa37f30a69fe5400e78ce83185938554cd3b61
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-04-30 09:30:36 +0000

    [Update]introductionバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,27 +2,28 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
   end
 
   def index
     @books = Book.all
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
-    @books = Book.new(book_params)
+    @books = Book.new
   end
 
   def update
@@ -34,9 +35,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
