c270fc8e3e7ed4edd5efcc1c78c86f392d539e10
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-05-02 03:13:52 +0000

    [Update]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about" => "homes#about", as: 'about'
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
-  resource :favorite, only: [:create, :destroy]
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
