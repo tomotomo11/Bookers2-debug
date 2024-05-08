09f3b5686abdcf1905807a59b543a9863329f4f6
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-05-08 04:24:20 +0000

    [Update]非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,10 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
